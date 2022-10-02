d133bf1698046c82bd49daf1a71702ec69645ab1
Syed Azeemuddin <azeemsrs99d@gmail.com>
2022-10-02 23:23:35 +0300

    1

--- htwoSum.py
+++ htwoSum.py
@@ -1,2 +1,18 @@
-#hashtables
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
 
+        m = {}
+        n = len(nums)
+
+        for i in range(0,n):
+            x = target - nums[i]
+            if x in m:
+                return [m[x],i]
+            
+            m[nums[i]] = i
+        
