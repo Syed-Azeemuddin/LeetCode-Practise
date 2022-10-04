09fa7f3cd17d015e42f1e5f4fe2a2d070b106069
Syed Azeemuddin <azeemsrs99d@gmail.com>
2022-10-04 14:01:32 +0300

    #49

---
+++ groupAnagrams.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution(object):
+
+    def findHash(i):
+        return "".join(sorted(i))
+
+    def groupAnagram(self, arr:List[str])->List[List[str]]:
+
+        m = {}
+        ls = []
+
+        for i in arr:
+            hash = self.findHash(i)
+            if hash not in m:
+                m[hash] = []
+            m[hash].append(i)
+        
+        for p in m.values():
+            ls.append(p)
